905c6a3b04e3bf46814491d2f05c2b6de87eb789
Yelda <yeldakuru99@outlook.com>
2025-04-01 16:30:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import PageContainer from './container/PageContainer'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+      <PageContainer>
+        <Header />
+      </PageContainer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/Header.css';
+function Header() {
+    return (
+        <div>
+            <div className='flex-row'>
+
+                <img className='logo' src="./src/images/logo.png" />
+                <p className='logo-text'>YELDA</p>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
